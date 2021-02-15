c755ae7b0427ad640ed9848e01c6fdf4180f8b69
Mayukh2007 <72680360+Mayukh2007@users.noreply.github.com>
2021-02-15 20:38:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+
+
+function preload(){
+  back_img = loadImage("jungle.jpg");
+  player_img = loadImage("basket2.png");
+  fruit1_img = loadImage("apple2.png");
+  fruit2_img = loadImage("banana2.png");
+  fruit3_img = loadImage("melon2.png");
+  fruit4_img = loadImage("orange2.png");
+  fruit5_img = loadImage("pineapple2.png");
+  fruitGroup = new Group();
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  background(back_img);
+   if(keyDown(LEFT_ARROW) && player.index !==null){
+    player1.x = player1.x - 10;
+   }
+   if(keyDown(RIGHT_ARROW) && player.index !==null){
+    player1.x = player1.x + 10;
+   }
+   if(keyDown("A") && player.index !==null){
+    player2.x = player2.x - 10;
+   }
+   if(keyDown("D") && player.index !==null){
+    player2.x = player2.x + 10;
+   }
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
+}
